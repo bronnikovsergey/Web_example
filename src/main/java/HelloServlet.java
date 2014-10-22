ffe4591ed6ce111e46ce9df6e3ffb154b7e06f96
bronnikovsergey <bronnikov_sergey@mail.ru>
2014-10-22 15:53:29 +0400

    making servlet

---
+++ src/main/java/HelloServlet.java
@@ -0,0 +1,35 @@
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class HelloServlet extends HttpServlet
+{
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+        throws ServletException, IOException
+    {
+        String q = req.getParameter("q");
+        PrintWriter out = resp.getWriter();
+
+        out.println("<html>");
+        out.println("<body>");
+        out.println("The parameter q was \"" + q + "\".");
+        out.println("</body>");
+        out.println("</html>");
+    }
+
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+        throws ServletException, IOException
+    {
+        String field = req.getParameter("field");
+        PrintWriter out = resp.getWriter();
+
+        out.println("<html>");
+        out.println("<body>");
+        out.println("You entered \"" + field + "\" into the text box.");
+        out.println("</body>");
+        out.println("</html>");
+    }
+}
