f764e24a8bb22ac26dbaaf97b1816e08c2050738
bronnikovsergey <bronnikov_sergey@mail.ru>
2014-10-31 03:22:05 +0400

    well done

--- src/main/java/HelloServlet.java
+++ src/main/java/HelloServlet.java
@@ -18,6 +18,7 @@ public class HelloServlet extends HttpServlet {
         PrintWriter out = resp.getWriter();
 
         ArrayList<String> files = Indexer.getInstance().search(field);
+
         out.println("<html>");
         out.println("<body>");
         for (String file : files) {
@@ -25,94 +26,5 @@ public class HelloServlet extends HttpServlet {
         }
         out.println("</body>");
         out.println("</html>");
-        /*
-        if (!field.equals("")) {
-
-
-            out.println("<html>");
-            out.println("<body>");
-            out.println("<p>The words: " + field + "</p>");
-
-            String[] pathToFile = new String[]{
-                    "src/main/TXTfiles/Arya.txt",
-                    "src/main/TXTfiles/Bran.txt",
-                    "src/main/TXTfiles/Catelyn.txt",
-                    "src/main/TXTfiles/Daenerys.txt",
-                    "src/main/TXTfiles/Eddard.txt",
-                    "src/main/TXTfiles/John.txt",
-                    "src/main/TXTfiles/Sansa.txt",
-                    "src/main/TXTfiles/Tyrion.txt"
-            };
-
-            String[] namesOfFiles = new String[]{
-                                "Arya.txt",
-                                "Bran.txt",
-                                "Catelyn.txt",
-                                "Daenerys.txt",
-                                "Eddard.txt",
-                                "John.txt",
-                                "Sansa.txt",
-                                "Tyrion.txt"
-                        };
-
-            String s;
-            String[] arrayString;
-            Map<String, HashSet<Integer>> map = new HashMap<String, HashSet<Integer>>();
-            HashSet<Integer> extra;
-
-
-            for (int i = 0; i < 8; i++) {
-                File someFile = new File(pathToFile[i]);
-                Scanner sc = new Scanner(someFile);
-
-                HashSet<Integer> set = new HashSet<Integer>();
-                set.add(i);
-
-                while (sc.hasNextLine()) {
-                    s = sc.nextLine();
-                    arrayString = s.split(" ");
-                    for (String word : arrayString) {
-                        if (map.containsKey(word)) {
-                            extra = map.get(word);
-                            HashSet<Integer> some = new HashSet<Integer>();
-                            some.addAll(extra);
-                            some.add(i);
-                            map.put(word, some);
-                        } else {
-                            map.put(word, set);
-                        }
-                    }
-                }
-            }
-
-            String[] fullField = field.split(" ");
-            boolean check = true;
-
-            for (String aString : fullField) {
-                if (!map.containsKey(aString)) {
-                    check = false;
-                }
-            }
-
-            if (check) {
-                HashSet<Integer> wSet = new HashSet<Integer>(map.get(fullField[0]));
-
-                for (String aFullField : fullField) {
-                    HashSet<Integer> intersect = new HashSet<Integer>(map.get(aFullField));
-                    wSet.retainAll(intersect);
-                }
-
-                for (Integer index : wSet) {
-                    out.println("<p>" + namesOfFiles[index] + "</p>");
-                }
-
-            } else {
-                out.println("<p> no words in all files </p>");
-            }
-
-        } else {
-            out.println("<p> empty form </p>");
-        }      */
-
     }
 }
--- src/main/java/Indexer.java
+++ src/main/java/Indexer.java
@@ -9,8 +9,9 @@ public enum Indexer {
     INSTANCE;
 
     private HashMap<String, HashSet<Integer>> invertIndex;
+    private File[] files;
 
-    private Indexer() throws FileNotFoundException {
+    private Indexer() {
 
         String str;
         String[] arrayString;
@@ -18,29 +19,35 @@ public enum Indexer {
         invertIndex = new HashMap<String, HashSet<Integer>>();
         HashSet<Integer> extra;
 
-        File f1 = new File(System.getProperty("user.dir") + "/src/main/TXTfiles");
-        File files[] = f1.listFiles();
-        for (int i = 0; i < files.length; i++) {
-            File file = files[i];
-            Scanner sc = new Scanner(file);
-            HashSet<Integer> set = new HashSet<Integer>();
-            set.add(i);
+        try {
+            File f1 = new File(System.getProperty("user.dir") + "/src/main/TXTfiles");
+            files = f1.listFiles();
+            assert files != null;
+            for (int i = 0; i < files.length; i++) {
+                File file = files[i];
 
-            while (sc.hasNextLine()) {
-                str = sc.nextLine();
-                arrayString = str.split(" ");
-                for (String word : arrayString) {
-                    if (invertIndex.containsKey(word)) {
-                        extra = invertIndex.get(word);
-                        HashSet<Integer> some = new HashSet<Integer>();
-                        some.addAll(extra);
-                        some.add(i);
-                        invertIndex.put(word, some);
-                    } else {
-                        invertIndex.put(word, set);
+                Scanner sc = new Scanner(file);
+                HashSet<Integer> set = new HashSet<Integer>();
+                set.add(i);
+
+                while (sc.hasNextLine()) {
+                    str = sc.nextLine();
+                    arrayString = str.split(" ");
+                    for (String word : arrayString) {
+                        if (invertIndex.containsKey(word)) {
+                            extra = invertIndex.get(word);
+                            HashSet<Integer> some = new HashSet<Integer>();
+                            some.addAll(extra);
+                            some.add(i);
+                            invertIndex.put(word, some);
+                        } else {
+                            invertIndex.put(word, set);
+                        }
                     }
                 }
             }
+        } catch (FileNotFoundException ex) {
+            // bad gateway
         }
     }
 
@@ -51,7 +58,6 @@ public enum Indexer {
     public ArrayList<String> search(String input) {
 
         ArrayList<String> result = new ArrayList<String>();
-
         if (!input.equals("")) {
             String[] inputArr = input.split(" ");
             boolean check = true;
@@ -71,22 +77,16 @@ public enum Indexer {
                 }
 
                 for (Integer index : wSet) {
-                    result.add("<p>" + index + "</p>");
-                    //out.println("<p>" + namesOfFiles[index] + "</p>");
+                    result.add("<p>" + files[index].getName() + "</p>");
                 }
 
             } else {
                 result.add("<p> no words in all files </p>");
-                //out.println("<p> no words in all files </p>");
             }
 
         } else {
             result.add("<p> empty form </p>");
-            //out.println("<p> empty form </p>");
         }
         return result;
-    }
-
-    public static void main(String[] args) {
     }
 }
